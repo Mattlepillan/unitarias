c37f8e26312cb05029b9019ec7c9207ffdcdd985
Matthew <ma.lepillan@duocuc.cl>
2025-06-10 21:47:47 -0400

    Nuevo commit

--- src/main/java/com/pruebas/unitarias/controller/MascotaController.java
+++ src/main/java/com/pruebas/unitarias/controller/MascotaController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pruebas.unitarias.model.Mascota;
 import com.pruebas.unitarias.service.MascotaService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,6 +40,22 @@ public class MascotaController {
         return mascotaService.obtenerMascotaPorId(id)
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.notFound().build());
+    }
+
+     @PutMapping("/{id}")
+    public ResponseEntity<Mascota> actualizar(@PathVariable Long id, @RequestBody Mascota mascota) {
+        try {
+            Mascota actualizada = mascotaService.actualizarMascota(id, mascota);
+            return ResponseEntity.ok(actualizada);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        mascotaService.eliminarMascota(id);
+        return ResponseEntity.noContent().build();
     }
     
     
